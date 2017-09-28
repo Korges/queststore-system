cef250bd57146814f68a3ed4a2c88beb789be57e
szajo <krzysztof.szajowski@gmail.com>
2017-09-28 10:35:47 +0200

    Add submission model

---
+++ src/main/java/models/Submission.java
@@ -0,0 +1,29 @@
+package models;
+
+public class Submission {
+
+    private boolean isMarked;
+    private Student student;
+    private Quest quest;
+
+    public Submission(Student student, Quest quest) {
+
+        this.isMarked = false;
+        this.student = student;
+        this.quest = quest;
+    }
+
+    public boolean setMarked() {
+
+        if (!isMarked) {
+
+            // todo enter grade, gib money
+            System.out.println("ENTER GRADE, (CHANGE IT TO OUTSIDE CLASS");
+            System.out.println("(INITIALIZE GIB MONEY TO STUDENT PLS)");
+            isMarked = true;
+
+            return true;
+        }
+        else return false;
+    }
+}
