295d3a067963da1abcc17e680788ef937e1cebd4
szajo <krzysztof.szajowski@gmail.com>
2017-09-28 19:27:15 +0200

    Implement QuestDAO

--- src/main/java/DAO/QuestDAO.java
+++ src/main/java/DAO/QuestDAO.java
@@ -1,34 +1,74 @@
 package DAO;
 
-import models.Artifact;
 import models.Quest;
+import models.Student;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class QuestDAO implements InterfaceDAO<Quest> {
 
+    private connectDB connect = DAO.connectDB.getInstance();
+
     private ArrayList<Quest> questList = new ArrayList<>();
 
-    public void add(Quest quest){
-        questList.add(quest);
+    public void add(Quest quest) {
+
+        String querry = String.format("INSERT INTO quests "+
+                "(name, description, value, experience, quest_category ) " +
+                "VALUES ('%s', '%s', '%d', '%d', '%s')", quest.getName(), quest.getDescription(), quest.getValue(), quest.getExperience(), quest.getCategory());
+        try {
+            connect.addRecord(querry);
+            System.out.println(querry);
+        } catch (SQLException e) {
+            System.out.println("Something get wrong");
+            System.exit(0);
+        }
+
+    }
+
+    private Quest createQuest(ResultSet result) throws SQLException {
+        Integer id = result.getInt("id");
+        String name = result.getString("name");
+        String description = result.getString("description");
+        Integer value = result.getInt("value");
+        Integer experience = result.getInt("experience");
+        String category = result.getString("quest_category");
+
+        return new Quest(id, name, description, value, experience, category);
     }
 
     public ArrayList get(){
+        ArrayList<Quest> questList = new ArrayList<>();
+        try {
+
+            ResultSet result = connect.getResult("SELECT * from quests;");
+            while (result.next()) {
+                Quest quest = createQuest(result);
+                questList.add(quest);
+
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
         return questList;
     }
 
+
     public void set(Quest quest) {
+        try {
+            String querry = String.format("UPDATE quests + " +
+             "SET name='%s',description = '%s',value = '%d', experience = '%d', category = '%s' " +
+             "WHERE id = %d", quest.getName(),quest.getDescription(),quest.getValue(),quest.getExperience(),quest.getCategory(), quest.getId());
+            connect.addRecord(querry);
 
-        for(Quest item : questList) {
-            System.out.println(item);
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
+
     }
 
-    public Quest getByName(String name) {
-        for (Quest quest : questList) {
-            if (quest.getName().equals(name)) {
-                return quest; // what if 2 quests with the very same name?
-            }
-        }
-        return null;
-    }
 }
