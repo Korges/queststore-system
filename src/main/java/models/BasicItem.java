276559a457c1408d18d8b56c442737a46c97a1ff
Korges <korgesik@gmail.com>
2017-09-28 17:09:03 +0200

    Add create new Artifact option.

--- src/main/java/models/BasicItem.java
+++ src/main/java/models/BasicItem.java
@@ -2,8 +2,13 @@ package models;
 
 public class BasicItem extends Artifact {
 
-    public BasicItem(Integer id, String name, String description, Integer price) {
+    public BasicItem(Integer id, String name, String description, Integer price, boolean isMagic) {
 
-        super(id, name, description, price);
+        super(id, name, description, price, isMagic);
+    }
+
+    public BasicItem(String name, String description, Integer price, boolean isMagic) {
+
+        super(name, description, price, isMagic);
     }
 }
--- src/main/java/models/MagicItem.java
+++ src/main/java/models/MagicItem.java
@@ -2,8 +2,13 @@ package models;
 
 public class MagicItem extends Artifact {
 
-    public MagicItem(Integer id, String name, String description, Integer price) {
+    public MagicItem(Integer id, String name, String description, Integer price, boolean isMagic) {
 
-        super(id, name, description, price);
+        super(id, name, description, price, isMagic);
+    }
+
+    public MagicItem(String name, String description, Integer price, boolean isMagic) {
+
+        super(name, description, price, isMagic);
     }
 }
