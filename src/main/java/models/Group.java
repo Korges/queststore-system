d1aeff84705e2a7ff86946e2649cf212f4d5cf48
szajo <krzysztof.szajowski@gmail.com>
2017-09-28 10:11:13 +0200

    Add group model

---
+++ src/main/java/models/Group.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Group {
+
+    private Integer id;
+    private String name;
+
+    public Group(Integer id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getID() {
+        return id;
+    }
+
+    public String toString(){
+
+        String view = String.format("ID: %s, Name: %s", this.getID(), this.getName());
+        return view;
+    }
+
+}
