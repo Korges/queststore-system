176cc1043cece53f0470d99a836bcb5d16079f57
Korges <korgesik@gmail.com>
2017-09-29 00:05:02 +0200

    Add Artifact new options. Add/Edit/Remove

--- src/main/java/DAO/ArtifactDAO.java
+++ src/main/java/DAO/ArtifactDAO.java
@@ -1,8 +1,11 @@
 package DAO;
 
+import java.sql.ResultSet;
 import models.Artifact;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import models.BasicItem;
+import models.MagicItem;
 
 public class ArtifactDAO implements InterfaceDAO<Artifact> {
 
@@ -17,12 +20,11 @@ public class ArtifactDAO implements InterfaceDAO<Artifact> {
         }
 
         String sql = String.format("INSERT INTO artifacts " +
-        "(price, description, is_magic)" +
-                "VALUES ('%d', '%s', '%s', '%d')", artifact.getName(), artifact.getPrice(), artifact.getDescription(), isMagic);
+        "(name, description, price, is_magic)" +
+                "VALUES ('%s', '%s', '%d', '%d')", artifact.getName(), artifact.getDescription(), artifact.getPrice(), isMagic);
 
         try {
             connect.addRecord(sql);
-            System.out.println(sql);
         } catch (SQLException e) {
             System.out.println("Something get wrong");;
             System.exit(0);
@@ -33,14 +35,60 @@ public class ArtifactDAO implements InterfaceDAO<Artifact> {
 
     public ArrayList get(){
         ArrayList<Artifact> artifactList = new ArrayList<>();
+        try {
+
+            ResultSet result = connect.getResult("SELECT * FROM artifacts");
+            while (result.next()) {
+
+                Integer  id = result.getInt("id");
+                String name = result.getString("name");
+                String description = result.getString("description");
+                Integer price = result.getInt("price");
+                Integer is_Magic = result.getInt("is_magic");
+                if(is_Magic.equals(1)){
+                    MagicItem newItem = new MagicItem(id, name, description, price, true);
+                    artifactList.add(newItem);
+                } else {
+                    BasicItem newItem = new BasicItem(id, name, description, price, false);
+                    artifactList.add(newItem);
+                }
+
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
         return artifactList;
     }
 
-    public void set(Artifact artifact) {
 
-//        for(Artifact item : artifactList){
-//            System.out.println(item);
-//        }
+    public void set(Artifact artifact) {
+        try {
+
+            Integer isMagic = 0;
+
+            if(artifact.getIsMagic()) {
+                isMagic = 1;
+            }
+            String sql = String.format("UPDATE artifacts SET name='%s',description = '%s',price = '%d', is_magic = '%d' WHERE id = %d",artifact.getName(),artifact.getDescription(),artifact.getPrice(),isMagic, artifact.getID());
+            connect.addRecord(sql);
+
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public void remove(Artifact artifact) {
+        try {
+            String sql = String.format("DELETE from artifacts WHERE id = %s", artifact.getID());
+            connect.addRecord(sql);
+
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+
     }
 
 
