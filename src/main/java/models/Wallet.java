24b19affa696a5f60b56570f9d6dee0dcbbcdb4d
Korges <korgesik@gmail.com>
2017-10-09 13:49:18 +0200

    Edit method substract method in Wallet.

--- src/main/java/models/Wallet.java
+++ src/main/java/models/Wallet.java
@@ -28,13 +28,10 @@ public class Wallet {
     }
 
 
-    boolean substract(Integer amount) {
+    public void substract(Integer amount) {
 
-        if (this.money >= amount) {
             this.money -= amount;
-            return true;
-        }
-        return false;
+
     }
 
 
