b1865fe0cfe92c8a043bbe765dc247573d7decf4
Korges <korgesik@gmail.com>
2017-10-10 10:05:34 +0200

    Create Inventory Model

---
+++ src/main/java/models/Inventory.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.util.StringJoiner;
+
+public class Inventory {
+
+    private Integer studentID;
+    private Integer artifactID;
+    private String date;
+    private String name;
+    private String description;
+    private Integer price;
+    private boolean isMagic;
+
+
+    public Inventory(Integer studentID, Integer artifactID, String date) {
+        this.studentID = studentID;
+        this.artifactID = artifactID;
+        this.date = date;
+    }
+
+    public Inventory(String name, String description, String date, Integer price, boolean isMagic) {
+        this.name = name;
+        this.description = description;
+        this.date= date;
+        this.price = price;
+        this.isMagic = isMagic;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public Integer getArtifactID() {
+        return artifactID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(" | ");
+        joiner.add("Name : " + name);
+        joiner.add("Description : " + description);
+        joiner.add("Date : " + date);
+        joiner.add("Price : " + price);
+        if(isMagic) {
+            joiner.add("Magic Item");
+        }
+        else {
+            joiner.add("Basic Item");
+        }
+        String joinedString = joiner.toString();
+
+        return joinedString;
+
+    }
+}
