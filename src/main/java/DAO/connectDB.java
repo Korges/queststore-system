d14ba00233fe31645c0a6bd1c17c3a53984de34e
Pablo <pablo.kulpa@gmail.com>
2017-10-10 13:53:52 +0200

    ConnectDB throw error .

--- src/main/java/DAO/connectDB.java
+++ src/main/java/DAO/connectDB.java
@@ -21,45 +21,28 @@ public class connectDB {
     }
 
 
-    public Connection connect(){
+    public Connection connect() throws SQLException{
 
         String url = "jdbc:sqlite:src/main/resources/db/database.db";
         Connection conn = null;
-        try{
-            conn = DriverManager.getConnection(url);
-
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
-        }
+        conn = DriverManager.getConnection(url);
         return conn;
     }
 
 
 
-    public ResultSet getResult(String sql) throws DAOException{
+    public ResultSet getResult(String sql) throws SQLException{
 
         ResultSet result = null;
-        try{
-
         Statement stmt = connection.createStatement();
         result = stmt.executeQuery(sql);
-        } catch (SQLException e) {
-            throw new DAOException("Problem with execute query");
-        }
-
         return result;
     }
 
 
-    public void addRecord(String sql) throws DAOException {
-        try{
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(sql);
-        } catch (SQLException e) {
-        throw new DAOException("Problem with execute query");
+    public void addRecord(String sql) throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate(sql);
     }
 
-
-    }
 }
