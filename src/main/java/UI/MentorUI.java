04dd10aa8e1efdcf21f3ae2535dffda4aa09588c
Korges <korgesik@gmail.com>
2017-10-10 10:06:46 +0200

    Add new Label and menuList (fundraise)

--- src/main/java/UI/MentorUI.java
+++ src/main/java/UI/MentorUI.java
@@ -13,11 +13,14 @@ public class MentorUI extends UI {
 
     public static String questMenuLabel = "MENTOR - Quest Menu";
 
+    public static String fundraiseMenuLabel = "MENTOR - Fundraise Menu";
+
     public static List<String> menuMainOptions = Arrays.asList(
         "Student Panel",
         "Artifact Panel",
         "Quest Panel",
         "See student wallet",
+        "Fundraise Panel",
         "EXIT");
 
     public static List<String> menuStudentOptions = Arrays.asList(
@@ -36,6 +39,13 @@ public class MentorUI extends UI {
 
     public static List<String> menuQuestOptions = Arrays.asList(
         "EXIT");
+
+    public static List<String> menuFundraiseOptions = Arrays.asList(
+        "Check Existing Fundraise",
+        "Execute Existing Fundraise",
+        "Delete Existing Fundraise",
+        "EXIT");
+
 
 
 
