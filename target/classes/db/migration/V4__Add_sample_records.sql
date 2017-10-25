ae36d61d64def592f291ee98515bcf6082961711
szajo <krzysztof.szajowski@gmail.com>
2017-10-25 15:10:13 +0200

    Add part3 refactor

---
+++ target/classes/db/migration/V4__Add_sample_records.sql
@@ -0,0 +1,9 @@
+DELETE FROM users;
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Krzysztof', 'Waligura', 's1@s.pl', 's1', 'Student', '2017.1');
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Paweł', 'Waleczny', 's2@s.pl', 's2', 'Student', '2017.1');
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Angela', 'Makrela', 'm1@m.pl', 'm1', 'Mentor', '2017.1');
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Andrzej', 'Doppa', 's4@s.pl', 's4', 'Student', '2017.1');
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Mateusz', 'Słaboręki', 's3@s.pl', 's3', 'Student', '2017.2');
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Juzef', 'Gebels', 'm2@m.pl', 'm2', 'Mentor', '2017.2');
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Teletubiś', 'Pedał', 'a@a.pl', 'a', 'Admin', NULL);
+INSERT INTO users (first_name, last_name, email, password, role, klass) VALUES ('Grzegorz', 'Brzęczyszczykiewicz', 's4@s.pl', 'ęą', 'Admin', NULL);
