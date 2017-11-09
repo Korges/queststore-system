1c68bdf3d8a010cc4328322e106a915b9e695b67
Pablo <pablo.kulpa@gmail.com>
2017-11-09 10:01:08 +0100

    Delete repeated method.

--- src/main/java/controller/Admin/CreateMentor.java
+++ src/main/java/controller/Admin/CreateMentor.java
@@ -5,6 +5,7 @@ import DAO.WebTemplateDao;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import controller.AdminController;
+import controller.helpers.ParseForm;
 
 import java.io.*;
 import java.net.URLDecoder;
@@ -21,13 +22,12 @@ public class CreateMentor implements HttpHandler {
         if (method.equals("GET")) {
             response = WebTemplate.getSiteContent("templates/admin/create-mentor.twig");
         }
-
         if (method.equals("POST")) {
             InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(),
                     "utf-8");
             BufferedReader br = new BufferedReader(isr);
             String formData = br.readLine();
-            Map<String,String> inputs = parseFormData(formData);
+            Map<String,String> inputs = ParseForm.parseFormData(formData);
             try{
                 AdminController adminController = new AdminController();
                 String firstName = inputs.get("first-name");
@@ -35,32 +35,17 @@ public class CreateMentor implements HttpHandler {
                 String email = inputs.get("email");
                 String password = inputs.get("password");
                 adminController.createMentor(firstName,lastName,password,email);
-                response =  WebTemplate.getSiteContent("templates/success.twig");
             }catch (SQLException e){
 
             }catch (NoSuchAlgorithmException e){
 
             }
-
-
-
+            response =  WebTemplate.getSiteContent("templates/success.twig");
         }
 
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
-    }
-
-    private Map<String,String> parseFormData(String formData) throws UnsupportedEncodingException {
-
-        Map<String, String> map = new HashMap<>();
-        String[] pairs = formData.split("&");
-        for(String pair : pairs){
-            String[] keyValue = pair.split("=");
-            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
-            map.put(keyValue[0], value);
-        }
-        return map;
     }
 }
