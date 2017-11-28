45cee8c8868abe870e536a348b03b86512e7d60c
Korges <korgesik@gmail.com>
2017-11-28 19:02:34 +0100

    Rework Store. Join all .java class in one file. Remove repeated code.

---
+++ src/main/java/controller/Student/Store/StorePanel.java
@@ -0,0 +1,78 @@
+package controller.Student.Store;
+
+import DAO.ArtifactDAO;
+import DAO.InventoryDAO;
+import models.Artifact;
+import models.Inventory;
+import models.Student;
+import UI.UI;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
+public class StorePanel {
+
+
+    public Integer getArtifactPrice(Integer artifactID){
+        Integer artifactPrice = null;
+        ArtifactDAO artifactDAO = null;
+        try {
+            artifactDAO = new ArtifactDAO();
+            artifactPrice = artifactDAO.getSingleArtifact(artifactID).getPrice();
+        } catch (SQLException e) {
+
+        }
+        return artifactPrice;
+    }
+
+
+    public boolean buyArtifact(Map<String, String> parsedForm, Student student) {
+
+        boolean status = false;
+        Integer artifactID = Integer.valueOf(parsedForm.get("id"));
+        Integer artifactPrice = getArtifactPrice(artifactID);
+        String date = UI.getCurrentDate();
+        InventoryDAO inventoryDAO = null;
+
+        try {
+
+            Inventory inventory = new Inventory(student.getID(), artifactID, date, artifactPrice);
+            inventoryDAO = new InventoryDAO();
+            inventoryDAO.add(inventory);
+            status = true;
+
+        } catch (SQLException e) {
+            return status;
+        }
+        return status;
+    }
+
+
+    public List<Artifact> getBasicItemList() {
+
+        List<Artifact> basicItemList = null;
+        try {
+            ArtifactDAO artfactDAO = new ArtifactDAO();
+            basicItemList = artfactDAO.getBasicItems();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return basicItemList;
+    }
+
+
+    public List<Inventory> getStudentInventoryList(Student student) {
+
+        List<Inventory> inventoryList = null;
+        try {
+            InventoryDAO inventoryDAO= new InventoryDAO();
+            inventoryList = inventoryDAO.getStudentInventory(student);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return inventoryList;
+    }
+}
+
